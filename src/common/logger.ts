63b3010974c17cae992ddcecae208fa4df2f40e6
darkgraycat <jburyanov@gmail.com>
2021-08-02 12:16:40 +0300

    update logger

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -1,19 +1,19 @@
 import chalk from 'chalk';
 
-export default {
-  log: (message: unknown): void => {
-    console.log(chalk.bgBlackBright.magentaBright(message));
-  },
-
-  error: (message: unknown): void => {
-    console.error(chalk.bgBlackBright.red(message));
-  },
-
-  warn: (message: unknown): void => {
-    console.warn(chalk.bgBlackBright.yellow(message));
-  },
-
-  info: (message: unknown): void => {
-    console.info(chalk.bgBlackBright.blueBright(message));
-  },
+const log = (message: unknown): void => {
+  console.log(chalk.bgBlackBright.magentaBright(message));
 };
+
+const error = (message: unknown): void => {
+  console.error(chalk.bgBlackBright.red(message));
+};
+
+const warn = (message: unknown): void => {
+  console.warn(chalk.bgBlackBright.yellow(message));
+};
+
+const info = (message: unknown): void => {
+  console.info(chalk.bgBlackBright.blueBright(message));
+};
+
+export default { log, error, warn, info };
