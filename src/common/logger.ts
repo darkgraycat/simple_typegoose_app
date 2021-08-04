0911217eb05894103c2a0cc79851be92ce2e3900
darkgraycat <jburyanov@gmail.com>
2021-08-04 08:01:56 +0300

    logger update

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -1,19 +1,19 @@
 import chalk from 'chalk';
 
 const log = (message: unknown): void => {
-  console.log(chalk.bgWhite.magentaBright(message));
+  console.log(chalk.magentaBright(message));
 };
 
 const error = (message: unknown): void => {
-  console.error(chalk.bgWhite.red(message));
+  console.error(chalk.red(message));
 };
 
 const warn = (message: unknown): void => {
-  console.warn(chalk.bgWhite.yellow(message));
+  console.warn(chalk.yellow(message));
 };
 
 const info = (message: unknown): void => {
-  console.info(chalk.bgWhite.blueBright(message));
+  console.info(chalk.blueBright(message));
 };
 
 export default { log, error, warn, info };
